9ba796d867423401e125a534de5931241c466864
Ruben Acuña <racuna1@asu.edu>
2025-10-01 23:11:18 +0800

    Added install scripts.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+apt-get -y install python3-pip
+# under (x)ubuntu 24.04.2 (LTS), this installs python 3.12.3
+
+cd /autograder/source
+
+# install packages for shoggoth-c
+pip3 install -r requirements.txt
